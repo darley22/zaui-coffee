9bf470abc872c3413a549314973c597e373f1e5c
Duong Nguyen <duongnguyen.22@icloud.com>
2026-02-21 13:39:21 +0700

    Cập nhật biểu tượng ở bottom.

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,23 +5,44 @@ import { MenuItem } from "types/menu";
 import { BottomNavigation, Icon } from "zmp-ui";
 import { CartIcon } from "./cart-icon";
 
+const HomeIconSvg = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+    <path d="M21 20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V9.48907C3 9.18048 3.14247 8.88917 3.38606 8.69972L11.3861 2.47749C11.7472 2.19663 12.2528 2.19663 12.6139 2.47749L20.6139 8.69972C20.8575 8.88917 21 9.18048 21 9.48907V20ZM19 19V9.97815L12 4.53371L5 9.97815V19H19Z"></path>
+  </svg>
+);
+
+const NotifIconSvg = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+    <path d="M5 18H19V11.0314C19 7.14806 15.866 4 12 4C8.13401 4 5 7.14806 5 11.0314V18ZM12 2C16.9706 2 21 6.04348 21 11.0314V20H3V11.0314C3 6.04348 7.02944 2 12 2ZM9.5 21H14.5C14.5 22.3807 13.3807 23.5 12 23.5C10.6193 23.5 9.5 22.3807 9.5 21Z"></path>
+  </svg>
+);
+
+const UserIconSvg = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+    <path d="M20 22H18V20C18 18.3431 16.6569 17 15 17H9C7.34315 17 6 18.3431 6 20V22H4V20C4 17.2386 6.23858 15 9 15H15C17.7614 15 20 17.2386 20 20V22ZM12 13C8.68629 13 6 10.3137 6 7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7C18 10.3137 15.3137 13 12 13ZM12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z"></path>
+  </svg>
+);
+
 const tabs: Record<string, MenuItem> = {
   "/": {
     label: "Trang chủ",
-    icon: <Icon icon="zi-home" />,
+    icon: <HomeIconSvg />,
+    activeIcon: <HomeIconSvg />
   },
   "/notification": {
     label: "Thông báo",
-    icon: <Icon icon="zi-notif" />,
+    icon: <NotifIconSvg />,
+    activeIcon: <NotifIconSvg />
   },
   "/cart": {
     label: "Giỏ hàng",
     icon: <CartIcon />,
-    activeIcon: <CartIcon active />,
+    activeIcon: <CartIcon active />
   },
   "/profile": {
     label: "Cá nhân",
-    icon: <Icon icon="zi-user" />,
+    icon: <UserIconSvg />,
+    activeIcon: <UserIconSvg />
   },
 };
 
